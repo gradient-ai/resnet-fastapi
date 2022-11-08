1dced8fd818433b75fdb0cb6223673d33f9611f1
paperspace <joshua@paperspace.com>
2022-11-08 22:04:26 +0000

    Moved setup function to resnet.py

--- main.py
+++ main.py
@@ -4,28 +4,9 @@ import torch
 from fastapi import FastAPI, Form, File, UploadFile
 
 from config import config
-from resnet import resnet18, resnet34, resnet50, resnet101, resnet152
+from resnet import resnet18, resnet34, resnet50, resnet101, resnet152, model_setup
 from preprocess import imgToTensor
 
-
-def model_setup():
-    MODEL_DIR = os.getenv('MODEL_DIR')
-    MODEL_FILE = os.getenv('MODEL_FILE')
-    MODEL_NAME = os.getenv('MODEL_NAME')
-    MODEL_PATH = os.path.join(MODEL_DIR, MODEL_FILE)
-
-    model_dict = model_dict = {'resnet18': resnet18(3,10)
-                    ,'resnet34': resnet34(3,10)
-                    ,'resnet50': resnet50(3,10)
-                    ,'resnet101': resnet101(3,10)
-                    ,'resnet152': resnet152(3,10)
-                    }
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = model_dict[MODEL_NAME]
-    model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
-
-    return model
 
 model = model_setup()
 prediction_classes = config.prediction_classes
@@ -34,7 +15,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World!"}
 
 
 @app.get("/predict")
--- resnet.py
+++ resnet.py
@@ -186,3 +186,23 @@ def resnet101(in_channels, n_classes, block=ResNetBottleNeckBlock, *args, **kwar
 
 def resnet152(in_channels, n_classes, block=ResNetBottleNeckBlock, *args, **kwargs):
     return ResNet(in_channels, n_classes, block=block, deepths=[3, 8, 36, 3], *args, **kwargs)
+
+
+def model_setup():
+    MODEL_DIR = os.getenv('MODEL_DIR')
+    MODEL_FILE = os.getenv('MODEL_FILE')
+    MODEL_NAME = os.getenv('MODEL_NAME')
+    MODEL_PATH = os.path.join(MODEL_DIR, MODEL_FILE)
+
+    model_dict = model_dict = {'resnet18': resnet18(3,10)
+                    ,'resnet34': resnet34(3,10)
+                    ,'resnet50': resnet50(3,10)
+                    ,'resnet101': resnet101(3,10)
+                    ,'resnet152': resnet152(3,10)
+                    }
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model_dict[MODEL_NAME]
+    model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
+
+    return model
